10e95ea38e4a77d1ddc0a7ca209395bf8483ffdc
Jonathan Martin <60260218+jonamartin@users.noreply.github.com>
2020-10-29 23:41:39 -0300

    About me section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './Components/Elements/nav-bar/nav-bar.component';
+import { AboutMeComponent } from './Components/Views/about-me/about-me.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
