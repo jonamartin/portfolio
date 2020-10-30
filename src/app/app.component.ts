3772d43ec13debc2a13ba163283086e0d0d39063
Jonathan Martin <60260218+jonamartin@users.noreply.github.com>
2020-10-30 00:19:19 -0300

    michalsnik/aos library and about me text.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare const AOS: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+  ngOnInit() {
+    AOS.init();
+  }
 }
