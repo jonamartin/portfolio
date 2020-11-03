64be8b2119b517196f2b80259f409d95ae4f47a8
Jonathan Martin <60260218+jonamartin@users.noreply.github.com>
2020-11-02 23:52:40 -0300

    Meta as a service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 import { darkModeService } from './services/dark-mode.service';
 declare const AOS: any;
 @Component({
@@ -7,8 +8,17 @@ declare const AOS: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor(public darkModeService: darkModeService) {
+  constructor(public darkModeService: darkModeService,
+              public metaService : Meta) {
     this.darkModeService.start();
+    metaService.addTag({property: 'og:title', content: 'Jonathan Martin'}, true);
+    metaService.addTag({property: 'og:description', content: 'Portfolio de Jonathan Martin, fullstack developer.'}, true);
+    metaService.addTag({property: 'og:url', content: 'develope.ar'}, true);
+    metaService.addTag({property: 'og:image', content: 'https://develope.ar/assets/profilepicture.png'}, true);
+    metaService.addTag({property: 'twitter:title', content: 'Jonathan Martin'}, true);
+    metaService.addTag({property: 'twitter:description', content: 'Portfolio de Jonathan Martin, fullstack developer.'}, true);
+    metaService.addTag({property: 'twitter:url', content: 'develope.ar'}, true);
+    metaService.addTag({property: 'twitter:image', content: 'https://develope.ar/assets/profilepicture.png'}, true);
   }
   title = 'portfolio';
   ngOnInit() {
