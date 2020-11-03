7ef0b58cd7250a88d672cd179f6a1afe33df1a02
Jonathan Martin <60260218+jonamartin@users.noreply.github.com>
2020-11-03 03:05:41 -0300

    Toggle language with texts

--- src/app/Components/Views/experience/experience.component.ts
+++ src/app/Components/Views/experience/experience.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { languageToggleService } from 'src/app/services/language-toggle.service';
 
 @Component({
   selector: 'app-experience',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
-  constructor() { }
+  constructor(public languageToggleService : languageToggleService) { }
 
   ngOnInit(): void {
   }
